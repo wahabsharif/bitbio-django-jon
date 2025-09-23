9912a551257cc97c5cff8dd5097bb66b8e3cb180
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-09-23 12:37:17 +0500

    password_validation

--- digiCells/digiCells/settings/base.py
+++ digiCells/digiCells/settings/base.py
@@ -90,9 +90,9 @@ WSGI_APPLICATION = "digiCells.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    # },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
